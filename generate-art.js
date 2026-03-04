5475ea7ba402ea9ae25ef04b8ef5a18a3caf6e17
Puccilillo <elio.mapping@gmail.com>
2026-03-04 17:28:58 +0100

    sbem

--- generate-art.js
+++ generate-art.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const CONFIG = {
   apiBase: "http://127.0.0.1:7860",
-  useRandomSeed: false,
+  useRandomSeed: true,
   outputDir: "./art",
   logDir: "./output",
   promptDumpDir: "./output/prompts",
